d87febc449f92155e935b8e47bb2b4cd58f4e77f
Rupesh Chinta <rupesh.ch97@gmail.com>
2018-05-08 02:20:42 -0400

    Cleaned up plotting code

--- mp3Test.py
+++ mp3Test.py
@@ -4,60 +4,31 @@ from numpy import fft
 from matplotlib import pyplot as plt
 
 N_FFT = 2**17
-
-vSound = AudioSegment.from_mp3("v1.mp3")
-
-# get raw audio data as a bytestring
-# check the following to make sure
-# but we are assuming 2 byte samples and 1 channel
-vRawData = vSound.raw_data
-
-vSamples = np.fromstring(vRawData, dtype=np.int16)
-print('time', len(vSamples))
-plt.subplot(321)
-plt.plot(vSamples)
-# plt.show()
-
-vfft = fft.rfft(vSamples, n=N_FFT)[:8192]
-print('frequency', len(vfft))
-plt.subplot(322)
-plt.plot(np.absolute(vfft))
-# plt.show()
-
-fSound = AudioSegment.from_mp3("f1.mp3")
+fs = 44100
 
 # get raw audio data as a bytestring
 # check the following to make sure
 # but we are assuming 2 byte samples and 1 channel
-fRawData = fSound.raw_data
+def samplesFromFile(fname):
+	sound = AudioSegment.from_mp3(fname)
+	rawData = sound.raw_data
+	return np.fromstring(rawData, dtype=np.int16)
 
-fSamples = np.fromstring(fRawData, dtype=np.int16)
-print('time', len(fSamples))
-plt.subplot(323)
-plt.plot(fSamples)
-# plt.show()
+def plotSamples(samples, sub=None, nfft=N_FFT):
+	plt.subplot(sub if sub else 121)
+	plt.plot(samples)
+	fftSamples = fft.rfft(samples, n=nfft)[:8192]
+	plt.subplot(sub+1 if sub else 122)
+	plt.plot(np.absolute(fftSamples))
+	if sub is None: plt.show()
 
-ffft = fft.rfft(fSamples, n=N_FFT)[:8192]
-print('frequency', len(ffft))
-plt.subplot(324)
-plt.plot(np.absolute(ffft))
-# plt.show()
+vSamples = samplesFromFile("v1.mp3")
+plotSamples(vSamples, sub=321)
 
-tSound = AudioSegment.from_mp3("t1.mp3")
+fSamples = samplesFromFile("f1.mp3")
+plotSamples(fSamples, sub=323)
 
-# get raw audio data as a bytestring
-# check the following to make sure
-# but we are assuming 2 byte samples and 1 channel
-tRawData = tSound.raw_data
+tSamples = samplesFromFile("t1.mp3")
+plotSamples(tSamples, sub=325)
 
-tSamples = np.fromstring(tRawData, dtype=np.int16)
-print('time', len(tSamples))
-plt.subplot(325)
-plt.plot(tSamples)
-# plt.show()
-
-tfft = fft.rfft(tSamples, n=N_FFT)[:8192]
-print('frequency', len(tfft))
-plt.subplot(326)
-plt.plot(np.absolute(tfft))
 plt.show()
